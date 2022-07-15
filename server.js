66972e822ce7c4eef33f44c0e28194af1af83135
burd5 <austinburdette5@gmail.com>
2022-07-15 16:57:16 -0600

    fixed cors errors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const PORT = 8000
+
+app.use(cors())
 
 app.get('/', (request,response) => {
     response.sendFile(__dirname + '/index.html')
