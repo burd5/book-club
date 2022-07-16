e0ced15163b45f97352f2931036a78d3cd282f95
burd5 <austinburdette5@gmail.com>
2022-07-16 13:27:30 -0600

    changed file placements

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const cors = require('cors')
 const PORT = 8000
 
 app.use(cors())
+app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', (request,response) => {
     response.sendFile(__dirname + '/index.html')
+    response.sendFile(__dirname + '/main.js')
 })
 
 app.get('/api/:authorName', (request, response) => {
